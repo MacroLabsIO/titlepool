8f1ff48e95da4ba886b11179b66882e6afe956b3
wizard <wizardlabsxyz@gmail.com>
2022-09-28 22:05:32 -0700

    A simple style override

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NavigationContainer from "./route";
 import "./App.css";
+import "./StyleOverrides.css";
 import { Context } from "./context/WalletAdaptex.tsx";
 import { AuthProvider } from "./context/AuthProvider";
 
